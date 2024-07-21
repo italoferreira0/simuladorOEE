1716997f316f670a08ec31a3d49e15e80ba7a2d4
Italo <italofsilva583@gmail.com>
2024-07-20 22:03:51 -0300

    Alterando Graficos OEE

--- src/components/Graficos.jsx
+++ src/components/Graficos.jsx
@@ -7,177 +7,44 @@ const BarChart = ({ Disponibilidade, Performance, Qualidade, OEE }) => {
   //Grafico de Disponibilidade
   var grafDisponibilidade = {
     chart: {
-      height: 250,
-      type: "radialBar",
+        height: 350,
+        type: 'radialBar',
     },
-
     series: [parseFloat(Disponibilidade.toFixed(2))],
-    colors: ["#20E647"],
-    plotOptions: {
-      radialBar: {
-        hollow: {
-          margin: 0,
-          size: "70%",
-          background: "#293450"
-        },
-        track: {
-          dropShadow: {
-            enabled: true,
-            top: 2,
-            left: 0,
-            blur: 4,
-            opacity: 0.15
-          }
-        },
-        dataLabels: {
-          name: {
-            offsetY: -10,
-            color: "#fff",
-            fontSize: "13px"  // nome interno
-          },
-          value: {
-            color: "#fff",
-            fontSize: "30px", //
-            show: true
-          }
-        }
-      }
-    },
-    fill: {
-      type: "gradient",
-      gradient: {
-        shade: "dark",
-        type: "vertical",
-        gradientToColors: ["#87D4F9"],
-        stops: [0, 100]
-      }
-    },
-    stroke: {
-      lineCap: "round"
-    },
-    labels: ["Disponibilidade"],
-    fontSize: "20px"
-  };
-
-  var chart = new ApexCharts(document.getElementById("grafDisponibilidade"), grafDisponibilidade);
-
-  chart.render();
+    labels: ['Disponibilidade'],
+  }
+  
+  var chartDisp = new ApexCharts(document.getElementById("grafDisponibilidade"), grafDisponibilidade);
+  
+  chartDisp.render();
   //______________________________________________________________________________________________
   // Grafico de Performance
   var grafPerformance = {
     chart: {
-      height: 280,
-      type: "radialBar",
+        height: 350,
+        type: 'radialBar',
     },
-
     series: [parseFloat(Performance.toFixed(2))],
-    colors: ["#20E647"],
-    plotOptions: {
-      radialBar: {
-        hollow: {
-          margin: 0,
-          size: "70%",
-          background: "#293450"
-        },
-        track: {
-          dropShadow: {
-            enabled: true,
-            top: 2,
-            left: 0,
-            blur: 4,
-            opacity: 0.15
-          }
-        },
-        dataLabels: {
-          name: {
-            offsetY: -10,
-            color: "#fff",
-            fontSize: "13px"
-          },
-          value: {
-            color: "#fff",
-            fontSize: "30px",
-            show: true
-          }
-        }
-      }
-    },
-    fill: {
-      type: "gradient",
-      gradient: {
-        shade: "dark",
-        type: "vertical",
-        gradientToColors: ["#87D4F9"],
-        stops: [0, 100]
-      }
-    },
-    stroke: {
-      lineCap: "round"
-    },
-    labels: ["Performance"]
-  };
-
-  var chart = new ApexCharts(document.getElementById("grafPerformance"), grafPerformance);
-
-  chart.render();
+    labels: ['Performance'],
+  }
+  
+  var chartPerf = new ApexCharts(document.getElementById("grafPerformance"), grafPerformance);
+  
+  chartPerf.render();
   //______________________________________________________________________________________________
   // Grafico de Qualidade
   var grafQualidade = {
     chart: {
-      height: 280,
-      type: "radialBar",
+        height: 350,
+        type: 'radialBar',
     },
-
     series: [parseFloat(Qualidade.toFixed(2))],
-    colors: ["#20E647"],
-    plotOptions: {
-      radialBar: {
-        hollow: {
-          margin: 0,
-          size: "70%",
-          background: "#293450"
-        },
-        track: {
-          dropShadow: {
-            enabled: true,
-            top: 2,
-            left: 0,
-            blur: 4,
-            opacity: 0.15
-          }
-        },
-        dataLabels: {
-          name: {
-            offsetY: -10,
-            color: "#fff",
-            fontSize: "13px"
-          },
-          value: {
-            color: "#fff",
-            fontSize: "30px",
-            show: true
-          }
-        }
-      }
-    },
-    fill: {
-      type: "gradient",
-      gradient: {
-        shade: "dark",
-        type: "vertical",
-        gradientToColors: ["#87D4F9"],
-        stops: [0, 100]
-      }
-    },
-    stroke: {
-      lineCap: "round"
-    },
-    labels: ["Qualidade"]
-  };
-
-  var chart = new ApexCharts(document.getElementById("grafQualidade"), grafQualidade);
-
-  chart.render();
+    labels: ['Qualidade'],
+  }
+  
+  var chartQua = new ApexCharts(document.getElementById("grafQualidade"), grafQualidade);
+  
+  chartQua.render();
   //______________________________________________________________________________________________
   // Grafico de OEE
 
@@ -186,13 +53,13 @@ const BarChart = ({ Disponibilidade, Performance, Qualidade, OEE }) => {
         height: 350,
         type: 'radialBar',
     },
-    series: [parseFloat(Qualidade.toFixed(2))],
+    series: [parseFloat(OEE.toFixed(2))],
     labels: ['OEE'],
   }
   
-  var chart = new ApexCharts(document.getElementById("grafOEE"), grafOEE);
+  var chartOEE = new ApexCharts(document.getElementById("grafOEE"), grafOEE);
   
-  chart.render();
+  chartOEE.render();
   return (
     <div>
       {/* <Chart options={options} series={series} type="bar" height={300} width={600}/> */}
